6f9e97fd102ae4d220a9acdbad6fdbf042c92f10
A-Salty-Fish <1309032417@qq.com>
2020-09-12 08:49:45 +0800

    Szh巨佬的多矩形绘制

--- Week1/CLASS/triangle.js
+++ Week1/CLASS/triangle.js
@@ -1,46 +1,58 @@
 
-var gl;
-var points;
+window.onload = function() {
+    // Get A WebGL context
+    var canvas = document.getElementById("canvas");
+    var gl = canvas.getContext("experimental-webgl");
 
-window.onload = function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+    // setup a GLSL program
+    var vertexShader = createShaderFromScriptElement(gl, "2d-vertex-shader");
+    var fragmentShader = createShaderFromScriptElement(gl, "2d-fragment-shader");
+    var program = createProgram(gl, [vertexShader, fragmentShader]);
+    gl.useProgram(program);
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+    // set color
+    var colorLocation = gl.getUniformLocation(program, "u_color");
 
+    // set the resolution
+    var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
 
-    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+    // Create a buffer
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.enableVertexAttribArray(resolutionLocation);
+    gl.vertexAttribPointer(resolutionLocation, 2, gl.FLOAT, false, 0, 0);
 
-    //  Configure WebGL
+    // draw 50 random rectangles in random colors
+    for (var ii = 0; ii < 50; ++ii) {
+        // Setup a random rectangle
+        setRectangle(
+            gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+        // Set a random color.
+        gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
 
-    //  Load shaders and initialize attribute buffers
+        // Draw the rectangle.
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+    }
+}
 
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+// Returns a random integer from 0 to range - 1.
+function randomInt(range) {
+    return Math.floor(Math.random() * range);
+}
 
-    // Load the data into the GPU
-
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER,vertices, gl.STATIC_DRAW );
-
-    // Associate out shader variables with our data buffer
-
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    render();
-
-
-};
-
-
-function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+// Fills the buffer with the values that define a rectangle.
+function setRectangle(gl, x, y, width, height) {
+    var x1 = x;
+    var x2 = x + width;
+    var y1 = y;
+    var y2 = y + height;
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2]), gl.STATIC_DRAW);
 }
