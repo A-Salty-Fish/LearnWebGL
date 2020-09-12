5fc7b6766e9561cedaa2e3f4df90ffc7f891c7e7
A-Salty-Fish <1309032417@qq.com>
2020-09-12 14:30:15 +0800

    红绿蓝三色随机三角形

--- Week1/CLASS/triangle.js
+++ Week1/CLASS/triangle.js
@@ -24,7 +24,7 @@ window.onload = function() {
     gl.vertexAttribPointer(resolutionLocation, 2, gl.FLOAT, false, 0, 0);
 
     // draw 50 random rectangles in random colors
-    for (var ii = 0; ii < 50; ++ii) {
+    for (var ii = 0; ii < 3; ++ii) {
         // Setup a random rectangle
         // setRectangle(
         //     gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
@@ -35,7 +35,11 @@ window.onload = function() {
         )
 
         // Set a random color.
-        gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
+        gl.uniform4f(colorLocation,
+            ii % 3 == 0 ? 1 : 0,
+            ii % 3 == 1 ? 1 : 0,
+            ii % 3 == 2 ? 1 : 0,
+            1);
 
         // Draw the rectangle.
         gl.drawArrays(gl.TRIANGLES, 0, 4);
@@ -45,21 +49,6 @@ window.onload = function() {
 // Returns a random integer from 0 to range - 1.
 function randomInt(range) {
     return Math.floor(Math.random() * range);
-}
-
-// Fills the buffer with the values that define a rectangle.
-function setRectangle(gl, x, y, width, height) {
-    var x1 = x;
-    var x2 = x + width;
-    var y1 = y;
-    var y2 = y + height;
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2]), gl.STATIC_DRAW);
 }
 
 function setTriangle(gl, x1, y1, x2, y2, x3, y3){
