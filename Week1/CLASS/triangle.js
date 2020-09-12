bd8bb15eff2543222b6f9bf7c4949fdbc2daaaec
A-Salty-Fish <1309032417@qq.com>
2020-09-12 14:49:03 +0800

    封装等边三角形函数

--- Week1/CLASS/triangle.js
+++ Week1/CLASS/triangle.js
@@ -1,7 +1,9 @@
+var canvas = document.getElementById("canvas");
+canvas_rect = canvas.getBoundingClientRect();
+canvas_width = canvas_rect.width
 
 window.onload = function() {
     // Get A WebGL context
-    var canvas = document.getElementById("canvas");
     var gl = canvas.getContext("experimental-webgl");
 
     // setup a GLSL program
@@ -23,27 +25,7 @@ window.onload = function() {
     gl.enableVertexAttribArray(resolutionLocation);
     gl.vertexAttribPointer(resolutionLocation, 2, gl.FLOAT, false, 0, 0);
 
-    // draw 50 random rectangles in random colors
-    for (var ii = 0; ii < 3; ++ii) {
-        // Setup a random rectangle
-        // setRectangle(
-        //     gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
-        setTriangle(gl,
-            randomInt(300), randomInt(300),
-            randomInt(300), randomInt(300),
-            randomInt(300), randomInt(300)
-        )
-
-        // Set a random color.
-        gl.uniform4f(colorLocation,
-            ii % 3 == 0 ? 1 : 0,
-            ii % 3 == 1 ? 1 : 0,
-            ii % 3 == 2 ? 1 : 0,
-            1);
-
-        // Draw the rectangle.
-        gl.drawArrays(gl.TRIANGLES, 0, 4);
-    }
+    drawTriColorEqTriangle(gl,colorLocation,canvas_width/2,canvas_width/2,canvas_width/2);
 }
 
 // Returns a random integer from 0 to range - 1.
@@ -57,4 +39,44 @@ function setTriangle(gl, x1, y1, x2, y2, x3, y3){
         x2, y2,
         x3, y3,
         ]), gl.STATIC_DRAW);
+}
+
+function setPartOfEqTriangle(gl,centerX,centerY,width,part){
+    switch (part){
+        case 0:
+            setTriangle(gl,
+                centerX, centerY,
+                centerX, centerY - width * Math.sqrt(3) /3,
+                centerX - width / 2, centerY + width * Math.sqrt(3)/6
+            )
+            break;
+        case 1:
+            setTriangle(gl,
+                centerX, centerY,
+                centerX - width / 2, centerY + width * Math.sqrt(3)/6,
+                centerX + width / 2, centerY + width * Math.sqrt(3)/6,
+            )
+            break;
+        case 2:
+            setTriangle(gl,
+                centerX, centerY,
+                centerX, centerY - width * Math.sqrt(3) /3,
+                centerX + width / 2, centerY + width * Math.sqrt(3)/6,
+            )
+            break;
+    }
+}
+function drawTriColorEqTriangle(gl, colorLocation, centerX, centerY, width){
+    for (var ii = 0; ii < 3; ++ii) {
+        // Setup a random rectangle
+        setPartOfEqTriangle(gl,centerX,centerY,width,ii%3);
+        // Set a random color.
+        gl.uniform4f(colorLocation,
+            ii % 3 == 0 ? 1 : 0,
+            ii % 3 == 1 ? 1 : 0,
+            ii % 3 == 2 ? 1 : 0,
+            1);
+        // Draw the rectangle.
+        gl.drawArrays(gl.TRIANGLES, 0, 4);
+    }
 }
