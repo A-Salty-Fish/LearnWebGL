9b11599616b405aaff1ef1c62518a790e1297d43
A-Salty-Fish <1309032417@qq.com>
2020-09-21 19:37:28 +0800

    画出实心三角形

--- Week2/CLASS/triangle.js
+++ Week2/CLASS/triangle.js
@@ -1,8 +1,9 @@
 var canvas = document.getElementById("canvas");
 canvas_rect = canvas.getBoundingClientRect();
 canvas_width = canvas_rect.width
-var points = [];
 
+var LengthPoints = [];//边数组
+var TrianglePoints = [];//三角形数组
 window.onload = function() {
     // Get A WebGL context
     var gl = canvas.getContext("experimental-webgl");
@@ -25,18 +26,15 @@ window.onload = function() {
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.enableVertexAttribArray(resolutionLocation);
     gl.vertexAttribPointer(resolutionLocation, 2, gl.FLOAT, false, 0, 0);
-    GenerateRecursiveTrianglePoints(0,200,40,5);
-    console.log(points);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
-    gl.drawArrays(gl.LINES, 0, points.length/2);
-    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
-    // gl.uniform4f(colorLocation,
-    //     1,
-    //     0,
-    //     0,
-    //     1
-    // );
-    // gl.drawArrays(gl.TRIANGLE_STRIP, 0, points.length);
+
+    GenerateTriangleLengthPoints(200,200,200,6);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(LengthPoints), gl.STATIC_DRAW);
+    gl.drawArrays(gl.LINES, 0, LengthPoints.length/2);
+    console.log(LengthPoints);
+    // GenerateTrianglePoints(200,200,200,6);
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(TrianglePoints), gl.STATIC_DRAW);
+    // gl.drawArrays(gl.TRIANGLES, 0, TrianglePoints.length/2);
+    // console.log(TrianglePoints);
 }
 
 //将三角形入缓冲区
@@ -47,19 +45,54 @@ function setTriangle(gl, x1, y1, x2, y2, x3, y3){
         x3, y3,
         ]), gl.STATIC_DRAW);
 }
-function GenerateRecursiveTrianglePoints(leftDownX, leftDownY, width, depth){
+//递归生成按边画的等边三角形顶点
+function GenerateRecursiveTriangleLengthPoints(leftDownX, leftDownY, width, depth){
     if (depth == 0) return;
     else{
-        points.push(leftDownX,leftDownY);
-        points.push(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2);
-        points.push(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2);
-        points.push(leftDownX+width,leftDownY);
-        points.push(leftDownX+width,leftDownY);
-        points.push(leftDownX,leftDownY);
+        LengthPoints.push(leftDownX,leftDownY);
+        LengthPoints.push(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2);
+        LengthPoints.push(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2);
+        LengthPoints.push(leftDownX+width,leftDownY);
+        LengthPoints.push(leftDownX+width,leftDownY);
+        LengthPoints.push(leftDownX,leftDownY);
 
+        GenerateRecursiveTriangleLengthPoints(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2, width,depth-1);
+        GenerateRecursiveTriangleLengthPoints(leftDownX+width, leftDownY, width, depth-1);
+    }
+}
+//生成由递归三角形组成的大等边三角形
+function GenerateTriangleLengthPoints(centerX,centerY,width,nums){//生成中心坐标为XY，宽度为X，边nums等分的空心三角形
+    LengthPoints = [];//清空边长数组
+    GenerateRecursiveTriangleLengthPoints(centerX - width/2,
+        centerY + width * Math.sqrt(3)/6,
+        width/nums,
+        nums);
+}
+//递归生成等边三角形顶点
+function GenerateRecursiveTrianglePoints(leftDownX, leftDownY, width, depth) {
+    if (depth == 0) return;
+    else{
+        TrianglePoints.push(leftDownX,leftDownY);
+        TrianglePoints.push(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2);
+        TrianglePoints.push(leftDownX+width,leftDownY);
+        //补齐空白
+        if (depth == 1){}
+        else {
+            TrianglePoints.push(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2);
+            TrianglePoints.push(leftDownX+width,leftDownY);
+            TrianglePoints.push(leftDownX+width*3/2,leftDownY-width*Math.sqrt(3)/2);
+        }
         GenerateRecursiveTrianglePoints(leftDownX+width/2,leftDownY-width*Math.sqrt(3)/2, width,depth-1);
         GenerateRecursiveTrianglePoints(leftDownX+width, leftDownY, width, depth-1);
     }
+}
+//生成由递归三角形组成的大等边三角形
+function GenerateTrianglePoints(centerX,centerY,width,nums){//生成中心坐标为XY，宽度为X，边nums等分的空心三角形
+    TrianglePoints = [];//清空三角形数组
+    GenerateRecursiveTrianglePoints(centerX - width/2,
+        centerY + width * Math.sqrt(3)/6,
+        width/nums,
+        nums);
 }
 //画三分之一部分的三角形
 function setPartOfEqTriangle(gl,centerX,centerY,width,part){
