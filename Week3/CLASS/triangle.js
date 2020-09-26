64eb9b83bcec19ded03e698824c547f84b32f707
A-Salty-Fish <1309032417@qq.com>
2020-09-26 10:24:07 +0800

    我的红色被吃了

--- Week3/CLASS/triangle.js
+++ Week3/CLASS/triangle.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById("canvas");
 canvas_rect = canvas.getBoundingClientRect();
 canvas_width = canvas_rect.width
-
+var program;
+var sqrt3 = Math.sqrt(3.0);
 window.onload = function() {
     // Get A WebGL context
     var gl = canvas.getContext("experimental-webgl");
@@ -9,11 +10,8 @@ window.onload = function() {
     // setup a GLSL program
     var vertexShader = createShaderFromScriptElement(gl, "2d-vertex-shader");
     var fragmentShader = createShaderFromScriptElement(gl, "2d-fragment-shader");
-    var program = createProgram(gl, [vertexShader, fragmentShader]);
+    program = createProgram(gl, [vertexShader, fragmentShader]);
     gl.useProgram(program);
-
-    // set color
-    var colorLocation = gl.getUniformLocation(program, "u_color");
 
     // set the resolution
     var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
@@ -25,17 +23,9 @@ window.onload = function() {
     gl.enableVertexAttribArray(resolutionLocation);
     gl.vertexAttribPointer(resolutionLocation, 2, gl.FLOAT, false, 0, 0);
 
-    // drawTriColorEqTriangle(gl,colorLocation,canvas_width/2,canvas_width/2,canvas_width/8);
-    // drawAGroupOfTriColorEqTriangle(gl,colorLocation,canvas_width/2,canvas_width/2,canvas_width/8);
-    // drawFourGroupOfTriColorEqTriangle(gl,colorLocation,canvas_width/2,canvas_width/2,canvas_width/8);
-    // drawAllGroupOfTriColorEqTriangle(gl,colorLocation,canvas_width/2,canvas_width/2 + canvas_width/8,canvas_width/8);
-    drawRecursiveTriangels(gl,colorLocation,canvas_width/2,canvas_width/2 + canvas_width/8,canvas_width,4);
+    drawTriangles(gl,canvas_width/2,canvas_width*5/8,canvas_width,4);
 }
 
-// Returns a random integer from 0 to range - 1.
-function randomInt(range) {
-    return Math.floor(Math.random() * range);
-}
 //将三角形入缓冲区
 function setTriangle(gl, x1, y1, x2, y2, x3, y3){
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
@@ -43,83 +33,32 @@ function setTriangle(gl, x1, y1, x2, y2, x3, y3){
         x2, y2,
         x3, y3,
         ]), gl.STATIC_DRAW);
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
-//画三分之一部分的三角形
-function setPartOfEqTriangle(gl,centerX,centerY,width,part){
-    switch (part){
-        case 0://red
-            setTriangle(gl,
-                centerX, centerY,
-                centerX, centerY - width * Math.sqrt(3) /3,
-                centerX - width / 2, centerY + width * Math.sqrt(3)/6
-            )
-            break;
-        case 1://green
-            setTriangle(gl,
-                centerX, centerY,
-                centerX - width / 2, centerY + width * Math.sqrt(3)/6,
-                centerX + width / 2, centerY + width * Math.sqrt(3)/6,
-            )
-            break;
-        case 2://blue
-            setTriangle(gl,
-                centerX, centerY,
-                centerX, centerY - width * Math.sqrt(3) /3,
-                centerX + width / 2, centerY + width * Math.sqrt(3)/6,
-            )
-            break;
-    }
-}
-//画三色的等边三角形
-function drawTriColorEqTriangle(gl, colorLocation, centerX, centerY, width){
-    for (var ii = 0; ii < 3; ++ii) {
-        // Setup a random rectangle
-        setPartOfEqTriangle(gl,centerX,centerY,width,ii%3);
-        // Set a random color.
-        gl.uniform4f(colorLocation,
-            ii % 3 == 0 ? 1 : 0,//red
-            ii % 3 == 1 ? 1 : 0,//green
-            ii % 3 == 2 ? 1 : 0,//blue
-            1);
-        // Draw the rectangle.
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
-    }
-}
-function drawRecursiveTriangels(gl, colorLocation, centerX, centerY, width, depth){
+function drawRecursiveTriangels(gl, x1,y1,x2,y2,x3,y3, depth){
     if (depth == 0)
-        drawTriColorEqTriangle(gl, colorLocation, centerX, centerY, width);
+        setTriangle(gl, x1,y1,x2,y2,x3,y3,);
     else {
-        drawRecursiveTriangels(gl, colorLocation,
-            centerX - width/4, centerY + width*Math.sqrt(3)/12,
-            width/2, depth-1)//left
-        drawRecursiveTriangels(gl, colorLocation,
-            centerX + width/4, centerY + width*Math.sqrt(3)/12,
-            width/2, depth-1)//right
-        drawRecursiveTriangels(gl, colorLocation,
-            centerX, centerY - width*Math.sqrt(3)/6,
-            width/2, depth-1)//up
-        drawRecursiveTriangels(gl, colorLocation, centerX, centerY, width/2, depth-1)//center
+        var x1x2 = (x1 + x2) / 2.0;
+        var y1y2 = (y1 + y2) / 2.0;
+        var x1x3 = (x1 + x3) / 2.0;
+        var y1y3 = (y1 + y3) / 2.0;
+        var x2x3 = (x2 + x3) / 2.0;
+        var y2y3 = (y2 + y3) / 2.0;
+        drawRecursiveTriangels(gl, x1,y1,x1x2,y1y2,x1x3,y1y3, depth-1);
+        drawRecursiveTriangels(gl, x2,y2,x2x3,y2y3,x1x2,y1y2, depth-1);
+        drawRecursiveTriangels(gl, x3,y3,x1x3,y1y3,x2x3,y2y3, depth-1);
     }
-
 }
-//画一组四个等边三角形
-function drawAGroupOfTriColorEqTriangle(gl, colorLocation, centerX, centerY, width){
-    drawTriColorEqTriangle(gl, colorLocation, centerX, centerY - width * Math.sqrt(3) /3, width);//up
-    drawTriColorEqTriangle(gl, colorLocation, centerX - width / 2, centerY + width * Math.sqrt(3)/6, width);//left
-    drawTriColorEqTriangle(gl, colorLocation, centerX + width / 2, centerY + width * Math.sqrt(3)/6, width);//right
-    drawTriColorEqTriangle(gl, colorLocation, centerX, centerY, width);//center
-}
-//画一大组三角形，包含四组一共十六个等边三角形
-function drawFourGroupOfTriColorEqTriangle(gl, colorLocation, centerX, centerY, width){
-    drawAGroupOfTriColorEqTriangle(gl, colorLocation, centerX, centerY - width * Math.sqrt(3) * 2 /3, width)//up
-    drawAGroupOfTriColorEqTriangle(gl, colorLocation, centerX - width, centerY + width * Math.sqrt(3) /3, width)//left
-    drawAGroupOfTriColorEqTriangle(gl, colorLocation, centerX + width, centerY + width * Math.sqrt(3) /3, width)//right
-    drawAGroupOfTriColorEqTriangle(gl, colorLocation, centerX, centerY, width)//center
-}
-//画出所有四个大组三角形
-function drawAllGroupOfTriColorEqTriangle(gl, colorLocation, centerX, centerY, width){
-    drawFourGroupOfTriColorEqTriangle(gl, colorLocation, centerX, centerY - width * Math.sqrt(3) * 4 /3, width)//up
-    drawFourGroupOfTriColorEqTriangle(gl, colorLocation, centerX - 2 * width, centerY + width * Math.sqrt(3) * 2 /3, width)//left
-    drawFourGroupOfTriColorEqTriangle(gl, colorLocation, centerX + 2 * width, centerY + width * Math.sqrt(3) * 2 /3, width)//right
-    drawFourGroupOfTriColorEqTriangle(gl, colorLocation, centerX, centerY, width)//center
+function drawTriangles(gl,centerX,centerY,width,depth){
+    var centerLocation = gl.getUniformLocation(program,"u_center");
+    gl.uniform2f(centerLocation,centerX,centerY);
+    var widthLocation = gl.getUniformLocation(program,"u_width");
+    gl.uniform1f(widthLocation,width);
+    drawRecursiveTriangels(gl,
+        centerX,centerY-width/sqrt3,
+        centerX-width/2,centerY+width*sqrt3/6.0,
+        centerX+width/2,centerY+width*sqrt3/6.0,
+        depth
+    )
 }
