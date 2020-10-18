7cc5e01fff3b107b125066ed50beee55eb4748e6
A-Salty-Fish <1309032417@qq.com>
2020-10-18 19:33:15 +0800

    画了一个奇怪多面体

--- Week6/CLASS/cubeq.js
+++ Week6/CLASS/cubeq.js
@@ -3,8 +3,6 @@
 var canvas;
 var gl;
 
-var NumVertices  = 36;
-
 var points = [];
 var colors = [];
 
@@ -24,7 +22,8 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    colorCube();
+    // colorCube();
+    colorSquare(0.5,5,4);
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
@@ -71,51 +70,71 @@ window.onload = function init()
     render();
 }
 
-function colorCube()
-{
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
-}
-var vertices = [
-    vec4( -0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5,  0.5,  0.5, 1.0 ),
-    vec4(  0.5,  0.5,  0.5, 1.0 ),
-    vec4(  0.5, -0.5,  0.5, 1.0 ),
-    vec4( -0.5, -0.5, -0.5, 1.0 ),
-    vec4( -0.5,  0.5, -0.5, 1.0 ),
-    vec4(  0.5,  0.5, -0.5, 1.0 ),
-    vec4(  0.5, -0.5, -0.5, 1.0 )
-];
+//作业部分
 function degToRad(deg){return deg * Math.PI / 180;}
 function lglt2xyz(longitude,latitude,radius){//经纬度转三维坐标
     var lg = degToRad(longitude) , lt = degToRad(latitude);
-    var y = radius * Math.sin(lt);
+    var z = radius * Math.sin(lt);
     var temp = radius * Math.cos(lt);
-    var x = temp * Math.sin(lg);
-    var z = temp * Math.cos(lg);
-    return vec4(z,x,y,1.0);
+    var y = temp * Math.sin(lg);
+    var x = temp * Math.cos(lg);
+    return vec4(x,y,z,1.0);
 }
 var TestVertices=[];
 function InitVertices(radius, columns ,rows){
-    TestVertices[0] = vec4(0,0,radius,1.0);
+    TestVertices[0] = vec4(0,0, -1 *radius,1.0);
     var index = 1;
-    var PerLatitude = 180 / (columns - 1);
-    var CurrentLatitude = PerLatitude;
+    var PerLatitude = 180 / ( columns -1 );
+    var CurrentLatitude = -90 +  PerLatitude;
     var PerLongitude = 360 / rows;
     for (var i =1 ; i < columns - 1 ; i++){
         var CureentLongitude = 0;
         for (var j=0;j<rows;j++){
+            // console.log(CureentLongitude+" "+CurrentLatitude);
             TestVertices[index] = lglt2xyz(CureentLongitude,CurrentLatitude,radius);
             CureentLongitude += PerLongitude;
             index ++;
         }
         CurrentLatitude += PerLatitude;
     }
-    TestVertices[index] = vec4(0,0,-1*radius,1.0);
+    TestVertices[index] = vec4(0,0,radius,1.0);
+}
+function colorSquare(radius,columns ,rows){
+    InitVertices(radius,columns,rows);
+    //第一层
+    for (var i = 1 ; i < rows ; i++){
+        MyTri(0,i,(i+1));
+    }
+    MyTri(0,rows,1);
+    //中间层
+    for (var c = 1 ; c < columns - 2; c++)
+    {
+        var BeginIndex = (c - 1) * rows + 1;
+        for (var i = 0 ; i < rows-1 ; i++){
+            MyQuad(BeginIndex+i,BeginIndex+i+rows,
+                BeginIndex+i+rows+1,BeginIndex+i+1);
+        }
+        MyQuad(c*rows,(c+1)*rows,
+            c*rows+1, BeginIndex);
+    }
+    //最后一层
+    var lastIndex = TestVertices.length - 1;
+    for (var i = 1 ; i < rows ; i++){
+        MyTri(lastIndex,lastIndex-i,lastIndex-i-1);
+        // points.push(TestVertices[lastIndex]);
+        // colors.push(vertexColors[lastIndex%8]);
+        // points.push(TestVertices[lastIndex-i]);
+        // colors.push(vertexColors[(lastIndex-i)%8]);
+        // points.push(TestVertices[lastIndex-i-1]);
+        // colors.push(vertexColors[(lastIndex-i-1)%8]);
+    }
+    MyTri(lastIndex,lastIndex-rows,lastIndex-1)
+    // points.push(TestVertices[lastIndex]);
+    // colors.push(vertexColors[lastIndex%8]);
+    // points.push(TestVertices[lastIndex-rows]);
+    // colors.push(vertexColors[(lastIndex-rows)%8]);
+    // points.push(TestVertices[(lastIndex-1)%8]);
+    // colors.push(vertexColors[(lastIndex-1)%8]);
 }
 var vertexColors = [
     [ 0.0, 0.0, 0.0, 1.0 ],  // black
@@ -127,8 +146,20 @@ var vertexColors = [
     [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
     [ 1.0, 1.0, 1.0, 1.0 ]   // white
 ];
-function quad(a, b, c, d)
-{
+function MyTri(a,b,c){
+    console.log("Tri "+a+" "+b+" "+c);
+    points.push(TestVertices[a]);
+    colors.push(vertexColors[a%8]);
+    points.push(TestVertices[b]);
+    colors.push(vertexColors[a%8]);
+    points.push(TestVertices[c]);
+    colors.push(vertexColors[a%8]);
+
+    console.log(TestVertices[a]);
+    console.log(TestVertices[b]);
+    console.log(TestVertices[c]);
+}
+function MyQuad(a, b, c, d) {
     // We need to parition the quad into two triangles in order for
     // WebGL to be able to render it.  In this case, we create two
     // triangles from the quad indices
@@ -136,9 +167,11 @@ function quad(a, b, c, d)
     //vertex color assigned by the index of the vertex
 
     var indices = [ a, b, c, a, c, d ];
-
+    console.log("Quad "+a+" "+b+" "+c+" "+d);
     for ( var i = 0; i < indices.length; ++i ) {
-        points.push( vertices[indices[i]] );
+        points.push( TestVertices[indices[i]] );
+        console.log(TestVertices[indices[i]]);
+        // if (TestVertices[indices[i]] == undefined) console.log((indices[i]));
         //colors.push( vertexColors[indices[i]] );
 
         // for solid colored faces use
@@ -154,7 +187,7 @@ function render()
     theta[axis] += 2.0;
     gl.uniform3fv(thetaLoc, theta);
 
-    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+    gl.drawArrays( gl.TRIANGLES, 0, points.length );
 
     requestAnimFrame( render );
 }
